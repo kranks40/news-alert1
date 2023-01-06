28040175ee620a9af8fd60745deaeb93f986d2f3
kranks40 <ketoranks29@gmail.com>
2023-01-06 11:49:47 -0400

    tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false,
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
